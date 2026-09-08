66996aac6878659a207c9faa143a957a91c33424
MaheshMahi-005 <gmaheshchandra75@gmail.com>
2026-09-08 17:21:51 +0530

    added routes with GET(id),PUT,DELETE and modified the POST router

--- routes/categories.py
+++ routes/categories.py
@@ -1,22 +1,61 @@
 from fastapi import APIRouter,HTTPException,status,Depends
 from sqlalchemy.orm import Session
 from database import get_db
-from schemas import CategoryCreate,CategoryResponse
+from auth import get_current_user
+from schemas import CategoryCreate,CategoryResponse,CategoryUpdate
 import models
 
 
 
 router = APIRouter(prefix='/categories',tags=['Categories'])
 
-
+#add new category
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=CategoryResponse)
-def add_category(category: CategoryCreate, db: Session = Depends(get_db)):
+def add_category(category: CategoryCreate, db: Session = Depends(get_db),current_user:models.User = Depends(get_current_user)):
+    existing = db.query(models.Category).filter(models.Category.name == category.name).first()
+    if existing:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail=f'{category.name} already exists')
     new_category = models.Category(name=category.name)
     db.add(new_category)
     db.commit()
     db.refresh(new_category)
     return new_category
 
+#get all categories
 @router.get('/', response_model=list[CategoryResponse])
 def get_all_categories(db: Session = Depends(get_db)):
     return db.query(models.Category).all()
+
+#get category by id
+@router.get('/{id}',response_model=CategoryResponse)
+def get_category_by_id(id:int,db:Session = Depends(get_db)):
+    db_category = db.query(models.Category).filter(models.Category.id == id).first()
+    if not db_category:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'{id} not found')
+    return db_category
+
+#update category by id
+@router.put('/{id}',response_model=CategoryResponse)
+def update_category_by_id(id:int,category:CategoryUpdate,db:Session=Depends(get_db),current_user:models.User = Depends(get_current_user)):
+    db_category = db.query(models.Category).filter(models.Category.id == id).first()
+    if not db_category:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'category_id:{id} not found')
+    db_category.name = category.name
+    db.add(db_category)
+    db.commit()
+    db.refresh(db_category)
+    return db_category
+
+#delete category by id
+@router.delete('/{id}')
+def delete_category(id:int,db:Session =Depends(get_db),curent_user:models.User = Depends(get_current_user)):
+    db_category = db.query(models.Category).filter(models.Category.id == id).first()
+    if not db_category:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'{id} not found')
+    #checks any products are using category
+    products = db.query(models.Product).filter(models.Product.category_id == id).count()
+    if products > 0:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail=f"cannot delete category '{db_category.name}': {products} product(s) still reference it")
+    db.delete(db_category)
+    db.commit()
+    return f'category_id:{id} deleted'
